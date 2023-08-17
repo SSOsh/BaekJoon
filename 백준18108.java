66b96e23abe95f542350656a7aa75e682782d5fa
SSOsh <tngh147258@kumoh.ac.kr>
2023-08-17 23:51:25 +0900

    Create 백준18108.java

---
+++ 백준18108.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(input - 543);
+    }
+}
